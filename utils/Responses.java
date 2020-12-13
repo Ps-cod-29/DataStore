d9ddc5f0c94393daa1619ada217ff858e02b113d
Ps-cod-29 <75925961+Ps-cod-29@users.noreply.github.com>
2020-12-13 12:43:45 +0530

    Create Responses.java

---
+++ utils/Responses.java
@@ -0,0 +1,17 @@
+package com.datastore.main.utils;
+
+
+public class Responses
+{
+	public static final String defaultdsLocation= "C:\\Users\\PS\\Desktop";
+	public static final int KEY_MAX_LENGTH = 32;
+	public static final int MILLISECONDS = 1000;
+	public static final String KEY_LENGTH_EXCEEDED = "Operation failed due to key length exceeded the limit of 32 chars)";
+	public static final String KEY_ALREADY_AVAILABLE = "Operation failed due to key already available";
+	public static final String KEY_NOT_AVAILABLE = "Operation failed due to key not available";
+	public static final String CREATE_SUCCESS = "Creation done successfully";
+	public static final String CREATE_FAILURE_ = "Creation failed";
+	public static final String READ_FAILURE= "Operation failed.Cannot Read at the moment";
+	public static final String DELETE_SUCCESS = "Deletion is done successfully";
+	public static final String DELETE_FAILURE = "Deletion failed";
+}
